88794e20a2027ea3764c0875c4ce65e5441f8cd0
Apteryx <apteryx.xyz@gmail.com>
2025-09-24 00:09:07 +1200

    Refactor `combineCommandOptions` to be simpler more and readable

--- packages/integration-carbon/src/utils/combine-command-options.ts
+++ packages/integration-carbon/src/utils/combine-command-options.ts
@@ -1,54 +1,39 @@
-// @ts-nocheck
-
-// NOTE: This is very hacky and is mostly a proof of concept, it doesn't cover every case where localisation might be needed, but it works
-
 import { Locale } from '@buape/carbon';
 
-const allowedLocales = Object.values(Locale);
+const ALLOWED_LOCALES = Object.values(Locale) as string[];
 
-export function combineCommandOptions<T extends object>(
-  localisationRecords: Record<string, T>,
-  defaultLocale: string,
+export function combineCommandOptions(
+  // biome-ignore lint/suspicious/noExplicitAny: allow to be anything
+  mappedOptions: Record<string, any>,
+  baseLocale: string,
 ) {
-  const locales = Object.keys(localisationRecords) //
-    .filter((l) => allowedLocales.includes(l) || l === defaultLocale);
-  const sources = locales.map((l) => localisationRecords[l]);
+  const options = structuredClone(mappedOptions[baseLocale]);
+  const availableLocales = Object.keys(mappedOptions) //
+    .filter((l) => l !== baseLocale && ALLOWED_LOCALES.includes(l));
 
-  function recurse<U>(values: U[]): T {
-    if (values.every((v) => Array.isArray(v))) {
-      return values[0].map((_, i) => recurse(values.map((v) => v?.[i] ?? {})));
-    }
-
-    if (values.every((v) => typeof v === 'object' && v !== null)) {
-      const keys = new Set(values.flatMap((v) => Object.keys(v || {})));
-      const result: Record<string, unknown> = {};
-
-      for (const key of keys) {
-        if (key === 'name' || key === 'description') {
-          const defaultValue = values[locales.indexOf(defaultLocale)]?.[key];
-          if (defaultValue !== undefined) result[key] = defaultValue;
-
-          const localisationKey = `${key}Localizations`;
-          const localisationMap: Record<string, unknown> = {};
-
-          for (let i = 0; i < values.length; i++) {
-            const current = values[i] as Record<string, unknown> | undefined;
-            if (current?.[key] !== undefined && locales[i] !== defaultLocale) {
-              localisationMap[locales[i]] = current[key];
-            }
-          }
-
-          result[localisationKey] = localisationMap;
-        } else {
-          result[key] = recurse(values.map((v) => v?.[key]));
-        }
+  for (const [key, value] of Object.entries(options)) {
+    if (key === 'name' || key === 'description') {
+      options[`${key}Localizations`] = {};
+      for (const locale of availableLocales) {
+        const other = mappedOptions[locale][key];
+        options[`${key}Localizations`][locale] = other;
       }
-
-      return result;
+      options[`${key}_localizations`] = options[`${key}Localizations`];
     }
 
-    return values[locales.indexOf(defaultLocale)];
+    if (Array.isArray(value)) {
+      for (const [index, option] of Object.entries(value)) {
+        const mapped = { [baseLocale]: option };
+        for (const locale of availableLocales) {
+          const other = mappedOptions[locale][key][index];
+          mapped[locale] = other;
+        }
+
+        const combined = combineCommandOptions(mapped, baseLocale);
+        options[key][index] = combined;
+      }
+    }
   }
 
-  return recurse(sources);
+  return options;
 }
