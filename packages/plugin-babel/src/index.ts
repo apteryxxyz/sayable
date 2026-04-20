544849858deb3e3247bc4c020c78fbf0f191aa56
Kodie <40654585+k0d13@users.noreply.github.com>
2026-04-21 05:16:56 +1200

    Fix babel plugin to use parserOverride for more reliable transformation (#14)

--- packages/plugin-babel/src/index.ts
+++ packages/plugin-babel/src/index.ts
@@ -1,29 +1,28 @@
 import { relative } from 'node:path';
-import type { PluginObj } from '@babel/core';
-import { parse } from '@babel/parser';
+import { type PluginObj, type parse as Parse } from '@babel/core';
 import { resolveConfig } from '@saykit/config/features/loader';
 
-export default function (): PluginObj {
+declare module '@babel/core' {
+  interface PluginObj {
+    parserOverride(code: string, opts: TransformOptions, parse: typeof Parse): ParseResult | null;
+  }
+}
+
+export default (): PluginObj => {
   const config = resolveConfig();
 
   return {
     name: 'saykit',
-    visitor: {
-      Program(path, state) {
-        const id_ = state.file.opts.filename;
-        if (!id_ || id_.includes('node_modules')) return;
-        const code = state.file.code ?? '';
+    visitor: {},
+    parserOverride(code, opts, parse) {
+      const id_ = opts.sourceFileName;
+      if (!id_ || id_.includes('node_modules')) return parse(code, opts);
 
-        const id = relative(process.cwd(), id_).replaceAll('\\', '/').split('?')[0]!;
-        const bucket = config.buckets.find((b) => b.match(id));
-        const transformed = bucket?.transformer.transform(code, id) ?? code;
+      const id = relative(process.cwd(), id_).replaceAll('\\', '/').split('?')[0]!;
+      const bucket = config.buckets.find((b) => b.match(id));
+      const transformed = bucket?.transformer.transform(code, id) ?? code;
 
-        if (transformed !== code) {
-          const ast = parse(transformed, state.file.opts.parserOpts as any);
-          path.replaceWith(ast.program);
-          path.skip();
-        }
-      },
+      return parse(transformed, opts);
     },
   };
-}
+};
